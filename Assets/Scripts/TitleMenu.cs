9656b383c7936ce648223ec546f34cce40ee7e95
yuruki <me@yuruki.net>
2024-08-21 19:26:14 -0700

    HOTFIX: Menus and lighting transitions
    - Unlock cursor when switching to main menu
    - Do not use baked lighting, and generate new lighting for shipScene

--- Assets/Scripts/TitleMenu.cs
+++ Assets/Scripts/TitleMenu.cs
@@ -8,7 +8,8 @@ public class TitleMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     // Update is called once per frame
